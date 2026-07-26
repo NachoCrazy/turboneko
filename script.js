8704da1634fed8bb3c1d9821022166a95418e6af
NachoCrazy <xkh2008@outlook.com>
2026-07-26 23:32:30 +0800

    Update rick roll

--- script.js
+++ script.js
@@ -205,9 +205,9 @@ document.addEventListener('DOMContentLoaded', () => {
             konamiIndex++;
             if (konamiIndex >= konamiCode.length) {
                 konamiIndex = 0;
-                // 触发彩蛋
+                // 触发彩蛋 — 直接播放
                 window.open(
-                    'https://www.bilibili.com/video/BV1GJ411x7h7',
+                    'https://vdse.bdstatic.com//192d9a98d782d9c74c96f09db9378d93.mp4',
                     '_blank'
                 );
             }
